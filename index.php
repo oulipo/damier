d0e3e313cdc983be9ec9edb5474a6d26af03680d
oulipo <laudesj@gmail.com>
2018-05-31 11:27:23 +0200

    améliorations diverses...

--- index.php
+++ index.php
@@ -2,19 +2,24 @@
     $colonnes = $_GET["colonnes"] ?? 10;
     $lignes = $_GET["lignes"] ?? 10;
 
-    function ligne($n, $blancEnPremier = true) {
+    $blanc = "<div class=\"case red\"></div>";
+    
+    $noir = "<div class=\"case black\"></div>";
+
+    function ligne($n, $white, $black, $blancEnPremier = true) {
         $ligne = "";
         for($i = 1; $i <= $n; $i++) {
-            $ligne .= $blancEnPremier ? "⬜" : "⬛";
+            //$ligne .= $blancEnPremier ? "⬜" : "⬛";
+            $ligne .= $blancEnPremier ? $white : $black;
             $blancEnPremier = ! $blancEnPremier;
         }
         return $ligne . "<br>";
     }
 
-    function damier($col, $lig) {
+    function damier($col, $white, $black, $lig) {
         $damier = "";
         for($i = 1; $i <= $lig; $i++) {
-            $damier .= ligne($col, ($i % 2) === 0);
+            $damier .= ligne($col, $white, $black, ($i % 2) === 0);
         }
         return $damier;
     }
@@ -27,14 +32,24 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= "Damier $colonnes x $lignes" ?></title>
     <style>
-        .damier {
-            font-size: 8px;
-            line-height: 8px;
-            letter-spacing: -3px;
+        .case {
+            display: inline-block;
+            width: 10px;
+            height:10px;
+        }
+
+        .red {
+            background-color: red;
+        }
+
+        .black {
+            background-color: black;
         }
     </style>
 </head>
 <body>
-    <div class="damier"><?= damier($colonnes, $lignes) ?></div>
+    <div class="damier"><?= damier($colonnes, $blanc, $noir, $lignes) ?></div>
+    <a href="">afficher un échiquier 8x8</a><br><!-- blanc et noir -->
+    <a href="">afficher un damier 10x10</a><br> <-- rouge et noir -->
 </body>
 </html>
